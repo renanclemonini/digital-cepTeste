f822dccb633ffe8efb494ee28e0651b8792eef3e
Renan Sotelo Clemonini <106163654+renanclemonini@users.noreply.github.com>
2023-07-05 21:34:19 -0300

    iniciando repositorio

---
+++ exemplo.php
@@ -0,0 +1,22 @@
+<?php 
+
+require_once './vendor/autoload.php';
+
+use Acer\DigitalCep\Search;
+
+$busca = new Search();
+
+$resultado = $busca->getAddressFromZipcode('42710400');
+
+$rua = $resultado['logradouro'];
+$bairro = $resultado['bairro'];
+$cidade = $resultado['localidade'];
+$uf = $resultado['uf'];
+
+$resultado['complemento'] = "Casa 6, lote 3";
+
+// echo "$rua fica no bairro $bairro em $cidade - $uf";
+
+print_r($resultado);
+
+?>
